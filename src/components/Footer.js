19f5435934eef6f01d369cd81842becd327114fc
Amy Shackles <amyshackles@gmail.com>
2019-03-23 22:26:39 -0700

    Started a Footer component to render application footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = props => {
+    return (
+        <div id="footer">
+            <p>Website created by Amy Shackles</p>
+            <p>Icons brought to you by Robohash.org</p>
+        </div>
+    )
+}
+
+export default Footer;
