6f7091da6eb065b25e72f76483174740f2e7e414
Amy Shackles <amyshackles@gmail.com>
2019-03-23 22:27:14 -0700

    Started writing Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = props => {
+    return (
+        <div id="header">
+            <h1>Cat Icons for Great Good</h1>
+        </div>
+    )
+}
+export default Header;
