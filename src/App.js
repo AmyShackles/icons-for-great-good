bac30ef3272a7aa9a5c7d5c2ede3af44340a2ed6
Amy Shackles <amyshackles@gmail.com>
2019-03-23 22:25:51 -0700

    Beginning to flesh out components for application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React from "react";
+import axios from "axios";
+import Header from "./components/Header.js";
+import Nav from "./components/Nav.js";
+import Footer from "./components/Footer.js";
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {image: ""};
+  }
+
+  componentDidMount() {
+    fetch("https://robohash.org/.png")
+    .then(response => {
+      this.setState({
+        image: response.url
+    });
+  }).catch(err => console.log(err.message));
+  }
   render() {
     return (
-      <div> </div>
-      //Reminder that this is what I was doing - https://robohash.org/
-     
+      <React.Fragment>
+        <div className="top-bar">
+        <Header />
+        <Nav />
+        </div>
+        <main>
+        <img src={this.state.image} alt="default robot icon" />
+        </main>
+        <Footer />
+        </React.Fragment>
     );
   }
 }
