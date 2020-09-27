f469aaf4b6bac1049f67cfef24e05fb56ae19823
Amy Shackles <amyshackles@gmail.com>
2020-09-26 22:31:30 -0700

    Adds robot.txt and hopefully fixes dropdown on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div id="footer" className="bg-info">
+        <div id="footer">
             <a className="text-white" href="https://amyshackles.com"><p>Website by Amy Shackles</p></a>
             <a className="text-white"href="https://robohash.org"><p>Robots lovingly delivered by Robohash.org</p></a>
         </div>
