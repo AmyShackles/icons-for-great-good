63e31d19c60ce7d8c6df868b197d518c044a4a94
Amy Shackles <amyshackles@gmail.com>
2021-03-07 17:23:58 -0800

    Simplifying logic for currentPage

--- src/App.js
+++ src/App.js
@@ -6,30 +6,67 @@ import { Cats } from "./components/Cats.js";
 import { Monster } from "./components/Monsters.js";
 import { DisembodiedRobotHead } from "./components/DisembodiedRobotHead.js";
 import { Robot } from "./components/Robot.js";
-import { Human } from './components/Human.js';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Human } from "./components/Human.js";
+import { Route, useLocation } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  const [currentPage, setCurrentPage] = React.useState();
-    return (
-      <>
-        <Router>
-          <div className="top-bar">
-          <Title iconType={currentPage}/>
-          <Nav />
-          </div>
-        <main id="main"className="container-fluid vh-100 py-3">
+    const [currentPage, setCurrentPage] = React.useState();
+    const { pathname } = useLocation();
+    React.useEffect(() => {
+        switch (pathname) {
+            case "/disembodied":
+                setCurrentPage("Disembodied Robot Head");
+                break;
+            case "/human":
+                setCurrentPage("Human");
+                break;
+            case "/robot":
+                setCurrentPage("Robot");
+                break;
+            case "/monster":
+                setCurrentPage("Monster");
+                break;
 
-            <Route exact path="/" render={() => <Cats setPage={() => setCurrentPage('Cat')} attribution="David Revoy"/>}/>
-            <Route path="/monster" render={() => <Monster setPage={() => setCurrentPage('Monster')} attribution="Hrvoje Novakovic"/>}/>
-            <Route path="/disembodied" render={() => <DisembodiedRobotHead setPage={() => setCurrentPage('Disembodied Robot Head')} attribution="Julian Peter Arias"/>}/>
-            <Route path="/robot" render={() => <Robot setPage={() => setCurrentPage('Robot')} attribution="Zikri Kader" />}/>
-            <Route path="/human" render={() => <Human setPage={() => setCurrentPage('Human')} attribution="Pablo Stanley"/>}/>
-        </main>
-        <Footer />
-        </Router>
+            default:
+                setCurrentPage("Cat");
+                break;
+        }
+    }, [pathname]);
+
+    return (
+        <>
+            <div className="top-bar">
+                <Title iconType={currentPage} />
+                <Nav />
+            </div>
+            <main id="main" className="container-fluid vh-100 py-3">
+                <Route
+                    exact
+                    path="/"
+                    render={() => <Cats attribution="David Revoy" />}
+                />
+                <Route
+                    path="/monster"
+                    render={() => <Monster attribution="Hrvoje Novakovic" />}
+                />
+                <Route
+                    path="/disembodied"
+                    render={() => (
+                        <DisembodiedRobotHead attribution="Julian Peter Arias" />
+                    )}
+                />
+                <Route
+                    path="/robot"
+                    render={() => <Robot attribution="Zikri Kader" />}
+                />
+                <Route
+                    path="/human"
+                    render={() => <Human attribution="Pablo Stanley" />}
+                />
+            </main>
+            <Footer />
         </>
     );
-}
-export { App }
+};
+export { App };
